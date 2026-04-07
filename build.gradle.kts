c97c94e7e71803b60e5ac774acb8c66859e6b0a9
Julia Rebouças <juliareboucasleite@gmail.com>
2026-04-07 20:33:13 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    application
 }
 
 group = "com.julia.discordbot"
@@ -11,7 +12,15 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("dev.kord:kord-core:0.12.0")
+
+    implementation("dev.kord:kord-core:0.18.1")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+    implementation("dev.kord:kord-core:0.8.0-M5")
+    implementation("org.slf4j:slf4j-api:1.6.1")
+    implementation("org.slf4j:slf4j-simple:1.6.1")
+
+    // Kotlin Logging
+    implementation("io.github.microutils:kotlin-logging:3.0.5")
 }
 
 kotlin {
@@ -20,4 +29,8 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("MainKt")
 }
